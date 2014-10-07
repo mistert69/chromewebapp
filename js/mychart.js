62ca2e20065529cb456be7b96db0fac91abaf7e4
mistert69 <karloust@yahoo.com>
2014-10-07 21:36:00 +0200

    added extra graphs

--- js/mychart.js
+++ js/mychart.js
@@ -26,7 +26,57 @@ nv.addGraph(function() {
         .tickFormat(d3.format("d"))
         ;
 
-    d3.select("svg")
+    d3.select("#raw svg")
+        .datum(myData())
+        .transition().duration(500).call(chart);
+
+    nv.utils.windowResize(
+            function() {
+                nv.log("resize");
+                chart.update();
+            }
+        );
+
+    return chart;
+});
+
+nv.addGraph(function() {
+    var chart = nv.models.lineChart();
+
+    chart.xAxis
+        .axisLabel("X-axis Label");
+
+    chart.yAxis
+        .axisLabel("Y-axis Label")
+        .tickFormat(d3.format("d"))
+        ;
+
+    d3.select("#fft svg")
+        .datum(myData())
+        .transition().duration(500).call(chart);
+
+    nv.utils.windowResize(
+            function() {
+                nv.log("resize");
+                chart.update();
+            }
+        );
+
+    return chart;
+});
+
+nv.addGraph(function() {
+    var chart = nv.models.lineChart();
+
+    chart.xAxis
+        .axisLabel("X-axis Label");
+
+    chart.yAxis
+        .axisLabel("Y-axis Label")
+        .tickFormat(d3.format("d"))
+        ;
+
+    d3.select("#time svg")
         .datum(myData())
         .transition().duration(500).call(chart);
 
